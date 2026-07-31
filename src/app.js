079189da5bc71edb5b46dc786135b478d7a5f0dd
iamsyedbilal <syedbilal.dev27@gmail.com>
2026-07-31 20:08:24 +0500

    Enhance user management APIs with improved error handling and response messages

--- src/app.js
+++ src/app.js
@@ -5,7 +5,8 @@ const User = require("./models/user");
 const app = express();
 app.use(express.json());
 
-app.post("/signup", function (req, res) {
+// API - Signup
+app.post("/signup", async function (req, res) {
   // const userObj = {
   //   firstName: "Syed",
   //   lastName: "Bilal",
@@ -13,22 +14,101 @@ app.post("/signup", function (req, res) {
   //   password: "bilal123",
   // };
 
+  const userObj = req.body;
+
+  if (!userObj) {
+    res.send("User cannot be empty").status(400);
+  }
+
   try {
-    const userObj = req.body;
-
-    if (!userObj) {
-      res.send("User cannot be empty").statusCode(400);
-    }
-
-    const user = User.create(userObj);
+    const user = await User.create(userObj);
 
     if (!user) {
-      res.send("Error while creating the user").statusCode(400);
+      res.send("Error while creating the user").status(400);
     }
 
-    res.send("User created successfully").statusCode(200);
+    res.send("User created successfully").status(200);
   } catch (error) {
-    res.send(`Error: user not created ${error}`).statusCode(400);
+    res.send(`Error: user not created ${error}`).status(400);
+  }
+});
+
+// API - GET User By Email
+app.get("/user", async function (req, res) {
+  const { emailId } = req.body;
+
+  if (!emailId) {
+    res.send("Email cannot be empty").status(400);
+  }
+
+  try {
+    const user = await User.find({ emailId });
+
+    if (!user) {
+      res.send("User not found").status(400);
+    }
+
+    res.send(user).status(200);
+  } catch (error) {
+    res.send(`Error: user not found ${error}`).status(400);
+  }
+});
+
+// API - GET All Users
+app.get("/feed", async function (req, res) {
+  try {
+    const user = await User.find();
+
+    if (!user) {
+      res.send("User not found").status(400);
+    }
+
+    res.send(user).status(200);
+  } catch (error) {
+    res.send(`Error: user not found ${error}`).status(400);
+  }
+});
+
+// API - Delete Users
+app.delete("/user", async function (req, res) {
+  const { userId } = req.body;
+
+  if (!userId) {
+    res.send("Invalid user id").status(400);
+  }
+
+  try {
+    await User.findByIdAndDelete({ _id: userId });
+
+    res.send("user deleted").status(201);
+  } catch (error) {
+    res.send(`Error: user not found ${error}`).status(400);
+  }
+});
+
+// API - Update User
+app.patch("/user", async function (req, res) {
+  const { userId } = req.body;
+  const data = req.body;
+
+  if (!userId) {
+    res.send("Invalid user id").status(400);
+  }
+
+  if (!data) {
+    res.send("data is required").status(400);
+  }
+
+  try {
+    const updatedUser = await User.findByIdAndUpdate(userId, data);
+
+    if (!updatedUser) {
+      res.send("user not updated").status(400);
+    }
+
+    res.send("user updated " + updatedUser).status(201);
+  } catch (error) {
+    res.send(`Error: user not update ${error}`).status(400);
   }
 });
 
